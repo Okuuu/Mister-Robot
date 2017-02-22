f6fb8d5c707c3656d67d168e504a842e3fbbcbb6
Robin Mesa Marin <robin.mesa-marin@eemi.com>
2017-02-22 10:26:33 +0100

    fix fire + bug

--- robocode.java
+++ robocode.java
@@ -8,25 +8,21 @@ public class mrRobot extends Robot {
             turnGunRight(360);
             turnGunRight(360);
             turnRight(90);
-            setBodyColor (Color red);
-            setBulletColor (Color black);
+            setBodyColor(Color red);
+            setBulletColor(Color black);
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        if (event.getDistance() < 100) {
-            fire(3);
-        }
-        else {
-            fire(1);
-        }
-    }
+
     }
 
-    public void onHitByBullet(HitByBulletEvent event){
+
+    public void onHitByBullet(HitByBulletEvent event) {
         back(200);
     }
 
-    public void onHitWall(HitWallEvent e){
+    public void onHitWall(HitWallEvent e) {
         turnRight(90);
     }
+}
