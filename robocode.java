709e73a626d7eed1d93077a5a3f219d4c6a08736
Victor de la Fouchardiere <victor.dlf@outlook.fr>
2017-02-22 12:13:17 +0100

    update

--- robocode.java
+++ robocode.java
@@ -1,50 +1,63 @@
 package man;
 import robocode.*;
 
-int getOthers;
-
 public class mrRobot extends Robot {
     public void run() {
-	
         while (true) {
-                ahead(100);
-                turnGunRight(360);
-                turnRight(120);
-                ahead(100);
-                turnGunRight(360);
-            }
+            ahead(20);
+            turnGunRight(360);
+			ahead(20);
+			turnGunRight(360);
+			ahead(20);
+			turnGunRight(360);
+			ahead(20);
+            turnRight(120);
         }
+    }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-
         double distance = e.getDistance();
 
         if(distance<200)
         {
-            fire(3.5);
+            fire(3);
+			turnGunRight(5);
+			fire(3);
+			turnGunRight(5);
+			fire(3);
         }
         else if(distance<500)
         {
             fire(2.5);
+			turnGunRight(5);
+			fire(2.5);
+			turnGunRight(5);
+			fire(2.5);
         }
         else if(distance<800)
         {
             fire(1.5);
+			turnGunRight(5);
+			fire(1.5);
+			turnGunRight(5);
+			fire(1.5);
         }
         else
         {
             fire(0.5);
+			fire(0.5);
+			fire(0.5);
         }
-		turnRight(30);
-		ahead(50);
+		turnRight(20);
+		ahead(30);
     }
 
-    public void onHitByBullet(HitByBulletEvent event){
-		turnRight(30);
-		ahead(50);
+    public void onHitByBullet(HitByBulletEvent event){	
+		turnRight(20);
+		ahead(20);
     }
 
-    public void onHitWall(HitWallEvent e){
-        turnRight(90);
+    public void onHitWall(HitWallEvent e){      
+		turnRight(30);
     }
 }
