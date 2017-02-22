9b8d04015ab3c04a41ba8f9301ee2a1e02a8b898
Robin Mesa Marin <robin.mesa-marin@eemi.com>
2017-02-22 09:49:00 +0100

    Update

--- robocode.java
+++ robocode.java
@@ -5,12 +5,19 @@ public class mrRobot extends Robot {
     public void run() {
         while (true) {
             ahead(100);
+            turnGunRight(360);
             back(100);
-
+            turnGunRight(360);
+            setBodyColor (Color.red);
+            setBulletColor (Color.black);
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+        fire(3);
+    }
+
+    public void onHitByBullet{
+        back(200);
     }
 }
